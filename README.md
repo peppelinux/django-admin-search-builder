3d6d3486751c45204ba8e3d3c2570674f8ea6a91
Giuseppe <giuseppe.demarco@unical.it>
2020-05-28 16:54:49 +0200

    setup.py

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ Django app that makes you able to build advanced queries via admin filters UI
 This Application aims to give you the faster and easier way to do advanced query via Django Admin ModelAdmin UI, using the Django's ORM.
 See example projects and gallery.
 
+### Setup
+
+````
+pip install django-admin-search-builder
+````
+
+in `settings.INSTALLED_APPS` add
+````
+    'admin_adv_search_builder',
+````
+
+in your ModelAdmin, use it following this example
+````
+from admin_adv_search_builder.filters import AdvancedSearchBuilder
+from django.contrib import admin
+
+from . models import Identity
+
+
+@admin.register(Identity)
+class IdentityAdmin(admin.ModelAdmin):
+    list_filter   = (AdvancedSearchBuilder,)
+````
+
 #### Example
 
 ````
@@ -18,3 +42,8 @@ cd example
 
 #### Gallery
 ![Alt text](images/1.png)
+
+
+#### Authors
+
+Giuseppe De Marco
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+#  rm -R build/ dist/ *egg-info
+#  python3 setup.py sdist
+#  twine upload dist/*
+
+setup(
+    name='django-admin-search-builder',
+    version='0.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description="Django Admin Search Builder",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/UniversitaDellaCalabria/django-admin-search-builder',
+    author='Giuseppe De Marco',
+    author_email='giuseppe.demarco@unical.it',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'django>=2.0,<4.0',
+    ],
+)
